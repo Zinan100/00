980f76ea3bff01327f53905d7979c65ae144ec91
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-15 14:39:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/deleterobot.git /deleterobot
+  git clone https://github.com/Zinan100/00.git /00
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /deleterobot
+  git clone $UPSTREAM_REPO /00
 fi
 cd /deleterobot
 pip3 install -U -r requirements.txt
